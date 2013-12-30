872b5ea266bb858fafd381055ed18beb7f719253
unknown <dextjox@DEXTJOX-RAFN9.Onyx.local>
2013-12-30 11:16:56 -0800

    'gtest2'

--- tasksclass.py
+++ tasksclass.py
@@ -3,33 +3,43 @@ import sqlite3
 import time
 class tasks:
 	def __init__(self):
-		self.ques=raw_input("new tasks?(ntask), completed a task?(comp), print tasks(ptask),delete a task?(d), view not completed tasks(nc), view completed tasks(c), add timer for task(t)???")
-		conn=sqlite3.connect('task4.db');
-		c=conn.cursor();
-		c.execute('''CREATE TABLE IF NOT EXISTS tasks (category text, task text, completed text(1), time decimal(6,2))''')
-		conn.commit()
-	def new_t(self):
-		catn=raw_input('What is the category of your task?');
-		taskn=raw_input('what is your task?')
+#		self.ques=raw_input("new tasks?(ntask), completed a task?(comp), print tasks(ptask),delete a task?(d), view not completed tasks(nc), view completed tasks(c), add timer for task(t)???")
+		self.conn=sqlite3.connect('task4.db');
+		self.c=self.conn.cursor();
+		self.c.execute('''CREATE TABLE IF NOT EXISTS tasks (category text, task text, completed text(1), time decimal(6,2))''')
+		self.conn.commit()
+
+		
+	def new_t(self,catn):
+#		catn=raw_input('What is the category of your task?');
+		#catn="j2"
+#		taskn=raw_input('what is your task?')
+		taskn='j'
 		compn='n';
-		tasktime=0;
-	#	c.execute(
-	#	conn.commit();
-		print "task %s has been created" %catn
-		quest=self.ques
-		return quest
-	def run(self):
-		quest=self.ques
-		if quest=='n':
-			print "new task"
-		elif quest=='t':
-			print "time"
-		else:
-			print "ok"
-			
+		timen=0;
+	 	self.c.execute('INSERT INTO tasks VALUES(?,?,?,?)', (catn, taskn,compn,timen))
+		self.conn.commit();
+	def poop(self):
+		print "poop"
+#		print "task %s has been created" %catn
+#		quest=self.ques
+#		return quest
+#	def run(self):
+#		quest=self.ques
+#		if quest=='n':
+#			print "new task"
+#		elif quest=='t':
+##			print "time"
+#		else:
+#			print "ok"
+##			
+
+
 d=tasks()
+
+
 #d.quest()
-d.run()
+#d.run()
 #d.ques
 
 
