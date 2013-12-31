d0797c77b1e57eceec434400b55fb004613eb356
unknown <dextjox@DEXTJOX-RAFN9.Onyx.local>
2013-12-30 16:55:45 -0800

    'guitest2'

--- guitest.py
+++ guitest.py
@@ -13,40 +13,47 @@ class tasksapp_tk(Tkinter.Tk):
 	#setup layout of grid	
 		self.grid()
 		#create widgets
-		self.varinit=Tkinter.StringVar()
-		self.messageinit=Tkinter.Message(self,textvariable=self.varinit, relief=Tkinter.RAISED, width=100)
-		self.messageinit.grid(column=0, row=1, sticky='EW')
-		self.varinit.set("Welcome to the task app")
+		self.messageinit=Tkinter.Message(self,text="Welcome to the task app", relief=Tkinter.RAISED, width=100)
+#		self.messageinit.grid(column=1, row=0, sticky='EW')
+		self.messageinit.pack()
 		
 		
 		#task list
 		self.var1=Tkinter.StringVar()
 		self.message1=Tkinter.Message(self,textvariable=self.var1, relief=Tkinter.RAISED, width=100)
-		self.message1.grid(column=0, row=2, sticky='EW')
+#		self.message1.grid(column=2, row=2, sticky='EW')
 		self.var1.set("tasks?")
+		self.message1.pack()
 		
 		#entry
 		#tasks.new_t(self.entry)
 		self.entry=Tkinter.Entry(self)
-		self.entry.grid(column=0,row=3,sticky='EW')
-		stuff=Tkinter.StringVar()
+		self.var2=Tkinter.StringVar()
+#		self.entry.bind("<Return>",self.OnButtonClick1)
+		self.entry.pack()
+
 		
-		stuff.set(self.entry.get)
-		self.entry.bind("<Return>", self.OnButtonClick1(stuff))
-		
-		#self.OnButtonClick1()
-		#tasks.new_t(stuff)
-		#tasks.new_t(self.entry)
-		
-#		button = Tkinter.Button(self, text=u"Click me!", command=self.OnButtonClick1)
-#		button.grid(column=2, row=3)
+		#print entry
+
+		self.message2=Tkinter.Message(self, width=10, text=self.entry.get())
+		self.message2.pack()
 		
 		
-		self.grid_columnconfigure(0,weight=1)
+		self.b=Tkinter.Button(self, text="get", width=10, command=self.OnButtonClick1)
+
+		self.b.pack()
+		
 		self.resizable(True, True)
-	
-	def OnButtonClick1(self, d):
-		print str(d)
+
+	def makemessage(self, **options):
+		message=Tkinter.Message(self, **options)
+		message.pack()
+		return message
+		
+	def OnButtonClick1(self):
+		new=self.makemessage(text=self.entry.get())
+		return new
+
 
 if __name__=="__main__":
 	app=tasksapp_tk(None)
