c47473870b5f734cf3f8b7d2e78146d93c0ee11d
dexter <aguila.dexter@gmail.com>
2014-01-13 17:18:59 -0800

    'guitest'

--- guitest.py
+++ guitest.py
@@ -30,30 +30,46 @@ class tasksapp_tk(Tkinter.Tk):
 		
 		#entry
 		#tasks.new_t(self.entry)
-		self.label=Tkinter.Label(self, text="Task 1 Category")
+		self.label=Tkinter.Label(self, text="Category")
 		self.entry=Tkinter.Entry(self)
 		self.var2=Tkinter.StringVar()
 		self.label.grid(column=0, row=1, sticky='EW')
 		self.entry.grid(column=1, row=1, sticky='EW')
-#		self.entry.bind("<Return>",self.OnButtonClick1)
+		self.entry.bind("<Return>",self.OnButtonClick1)
 #		self.entry.pack()
 
 		#fake button
-		self.b=Tkinter.Button(self, text="get", width=10, command=self.OnButtonP)
-		self.b.grid(column=0, row=4, sticky='EW')
+
 	#	self.b.pack()		
 		
 		#entry2
-		self.labelentry2=Tkinter.Label(self, text='Task 1')
+		self.labelentry2=Tkinter.Label(self, text='Task')
 		self.entry2=Tkinter.Entry(self)
 		self.labelentry2.grid(column=0, row=2, sticky='EW')
 		self.entry2.grid(column=1, row=2, sticky='EW')
 		
+		#submit new tasks
+		self.submit=Tkinter.Button(self, text='submit new', width=10, command=self.OnButtonSubmit)
+		self.submit.grid(column=2, row=2, sticky='EW')
+		
+		#Complete entry
+		self.labelentry3=Tkinter.Label(self, text='Rowid(for completed or Delete)')
+		self.entry3=Tkinter.Entry(self)
+		self.labelentry3.grid(column=0, row=3, sticky='EW')
+		self.entry3.grid(column=1, row=3, sticky='EW')
+		comprow=self.entry3.get()
+		self.button3=Tkinter.Button(self, text="Comp", width=5, command=self.Compbutton)
+		self.button3.grid(column=2, row=3, sticky='EW')
+		self.button4=Tkinter.Button(self, text="Del", width=5, command=self.Delbutton)
+		self.button4.grid(column=3, row=3, sticky='EW')		
+		
+		
 		#self.entry2.pack()
 		
 		#submit button
-		self.submit=Tkinter.Button(self, text='submit', width=10, command=self.OnButtonSubmit)
-		self.submit.grid(column=3, row=3, sticky='EW')
+
+		self.b=Tkinter.Button(self, text="refresh list", width=10, command=self.OnButtonP)
+		self.b.grid(column=0, row=6, sticky='EW')
 		#self.submit.pack()
 		#print entry
 
@@ -61,8 +77,8 @@ class tasksapp_tk(Tkinter.Tk):
 		#self.message2.grid(row=5, column=1, sticky='EW')	
 		self.resizable(True, True)
 
-	def makemessage(self, **options):
-		message=Tkinter.Message(self, **options)
+	def makemessage(self,tkinst, **options):
+		message=Tkinter.Message(tkinst, **options)
 		message.pack()
 		return message
 		
@@ -71,20 +87,52 @@ class tasksapp_tk(Tkinter.Tk):
 		return new
 	def OnButtonSubmit(self):		
 		sub=self.tasks.new_t(self.entry.get(),self.entry2.get())
+		self.entry.delete(0, Tkinter.END)
+		self.entry2.delete(0, Tkinter.END)
 		return sub
 	def OnButtonP(self):
-		conn=sqlite3.connect('task4.db')
+		conn=sqlite3.connect('task.db')
 		conn.text_factory = str
 		c=conn.cursor()
-		c.execute('select rowid, category, task, completed, time from tasks')
+		c.execute('select rowid, category, task, tasktime from tasks WHERE completed="n"')
+		#c.execute('select rowid, category, task, completed, tasktime from tasks')
+		root=Tkinter.Tk()
+		
 		p=c.fetchall()
-		i=5
+
+		
+		i=1
 		for row in p:
-			n=self.makemessage(text=row)
-			n.grid(column=0, row=i, sticky='EW')
+			#n=self.makemessage(text=row)
+			rlabel=self.makemessage(root,text='rowid', width=100)
+			rlabel.grid(column=0, row=0)
+			catlabel=self.makemessage(root,text='category', width=100)
+			catlabel.grid(column=1, row=0)
+			tlabel=self.makemessage(root,text='task')
+			tlabel.grid(column=2, row=0)
+			tmlabel=self.makemessage(root,text='time')
+			tmlabel.grid(column=3, row=0)
+			
+			j=0
+			for	tup in row:
+
+				n=self.makemessage(root, text=row[j], width=100)
+				n.grid(column=j, row=i)
+				j=j+1
 			i=i+1
+		root.mainloop()
 		print p
 		return p
+	def Compbutton(self):
+		comp=self.tasks.comp_t(self.entry3.get())
+		self.entry3.delete(0, Tkinter.END)
+		return comp
+	
+	def Delbutton(self):
+		delbut=self.tasks.delete_t(self.entry3.get())
+		self.entry3.delete(0, Tkinter.END)
+		return delbut
+		
 #	def listtable(self):
 #		self.table=
 
